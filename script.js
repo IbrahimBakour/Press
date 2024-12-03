87e5391346ec2618e7482278d323fd7db7401b9b
IbrahimBakour <68356802+IbrahimBakour@users.noreply.github.com>
2024-12-03 23:37:03 +0800

    filtered pics

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const filterButtons = document.querySelectorAll(".buttons button");
+const filterableCards = document.querySelectorAll(".section-content .card");
 
 menuOpenButton.addEventListener("click", () => {
   document.body.classList.toggle("show-mobile-menu");
@@ -11,6 +13,25 @@ menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 navLinks.forEach((link) => {
   link.addEventListener("click", () => menuOpenButton.click());
 });
+
+const filterCards = (e) => {
+  document.querySelector(".active").classList.remove("active");
+  e.target.classList.add("active");
+
+  filterableCards.forEach((card) => {
+    card.classList.add("hide");
+    if (
+      card.dataset.name === e.target.dataset.name ||
+      e.target.dataset.name === "all"
+    ) {
+      card.classList.remove("hide");
+    }
+  });
+};
+
+filterButtons.forEach((button) =>
+  button.addEventListener("click", filterCards)
+);
 
 const swiper = new Swiper(".slider-wrapper", {
   loop: true,
